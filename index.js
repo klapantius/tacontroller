8383075f437a58b75ed11578860e9e3d653a3e6a
Juhasz <balazs.juhasz@siemens.com>
2017-07-16 13:18:06 +0200

    format document

--- index.js
+++ index.js
@@ -4,19 +4,19 @@
 var Tail = require('always-tail2');
 var fs = require('fs');
 var filename = "/bj/temp/tail.txt";
- 
+
 if (!fs.existsSync(filename)) fs.writeFileSync(filename, "");
- 
+
 var tail = new Tail(filename, '\n');
- 
-tail.on('line', function(data) {
-  console.log("got line:", data);
-  broadcast(data);
-  data='';
+
+tail.on('line', function (data) {
+    console.log("got line:", data);
+    broadcast(data);
+    data = '';
 });
 
-tail.on('error', function(data) {
-  console.log("error:", data);
+tail.on('error', function (data) {
+    console.log("error:", data);
 });
 
 tail.watch();
@@ -36,22 +36,22 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('new client', function() {
-    console.log('a new client connected: ' + socket.id);
-    //todo: send all TA records to the new client
-  });
+io.on('connection', function (socket) {
+    socket.on('new client', function () {
+        console.log('a new client connected: ' + socket.id);
+        //todo: send all TA records to the new client
+    });
 });
 
-function broadcast(data){
+function broadcast(data) {
     io.emit('status change', data)
 }
 
-http.listen(3000, function(){
-  console.info("you can open now http://localhost:3000/")
+http.listen(3000, function () {
+    console.info("you can open now http://localhost:3000/")
 });
 //----------------------------------------------------------------------
